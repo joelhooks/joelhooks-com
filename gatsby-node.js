b643b575e98353a905c5fe28d390d2448fbb7f17
Joel Hooks <joel@egghead.io>
2019-01-02 16:15:26 -0800

    add old posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,47 +1,6 @@
 const path = require('path');
 const componentWithMDXScope = require('gatsby-mdx/component-with-mdx-scope');
 
-const PAGINATION_OFFSET = 2;
-
-const pluckCategories = edges =>
-  Object.keys(
-    edges.reduce((acc, value) => {
-      value.node.fields.categories.forEach(category => {
-        if (!acc[category]) {
-          acc[category] = category;
-        }
-      });
-
-      return acc;
-    }, {}),
-  );
-
-const groupByCategory = edges =>
-  edges.reduce((acc, value) => {
-    value.node.fields.categories.forEach(category => {
-      if (!acc[category]) {
-        acc[category] = [];
-      }
-      acc[category].push(value);
-    });
-    return acc;
-  }, {});
-
-const createCategoryPages = (createPage, edges) => {
-  const categories = pluckCategories(edges);
-
-  const posts = groupByCategory(edges);
-
-  Object.keys(posts).forEach(category => {
-    createPaginatedPages(
-      createPage,
-      posts[category],
-      `/categories/${category}`,
-      { categories, activeCategory: category },
-    );
-  });
-};
-
 const createPosts = (createPage, edges) => {
   edges.forEach(({ node }, i) => {
     const prev = i === 0 ? null : edges[i - 1].node;
@@ -58,53 +17,6 @@ const createPosts = (createPage, edges) => {
         id: node.id,
         prev,
         next,
-      },
-    });
-  });
-};
-
-const createBlog = (createPage, edges) => {
-  const categories = pluckCategories(edges);
-
-  createPaginatedPages(createPage, edges, '/blog', { categories });
-};
-
-const createPaginatedPages = (
-  createPage,
-  edges,
-  pathPrefix,
-  context,
-) => {
-  const pages = edges.reduce((acc, value, index) => {
-    const pageIndex = Math.floor(index / PAGINATION_OFFSET);
-
-    if (!acc[pageIndex]) {
-      acc[pageIndex] = [];
-    }
-
-    acc[pageIndex].push(value.node.id);
-
-    return acc;
-  }, []);
-
-  pages.forEach((page, index) => {
-    const previousPagePath = `${pathPrefix}/${index + 1}`;
-    const nextPagePath =
-      index === 1 ? pathPrefix : `${pathPrefix}/${index - 1}`;
-
-    createPage({
-      path: index > 0 ? `${pathPrefix}/${index}` : `${pathPrefix}`,
-      component: path.resolve(`src/templates/blog.js`),
-      context: {
-        pagination: {
-          page,
-          nextPagePath: index === 0 ? null : nextPagePath,
-          previousPagePath:
-            index === pages.length - 1 ? null : previousPagePath,
-          pageCount: pages.length,
-          pathPrefix,
-        },
-        ...context,
       },
     });
   });
@@ -137,9 +49,7 @@ exports.createPages = ({ actions, graphql }) =>
 
     const { edges } = data.allMdx;
 
-    createBlog(actions.createPage, edges);
     createPosts(actions.createPage, edges);
-    createCategoryPages(actions.createPage, edges);
   });
 
 exports.onCreateWebpackConfig = ({ actions }) => {
