cac519e3d0c8fb589b940ad45d76f231d3869ed5
Joel Hooks <joelhooks@gmail.com>
2019-01-04 22:13:03 -0800

    broken demo

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,14 @@
-const path = require('path');
+const path = require('path')
 
-const _ = require('lodash');
-const paginate = require('gatsby-awesome-pagination');
-const PAGINATION_OFFSET = 7;
+const _ = require('lodash')
+const paginate = require('gatsby-awesome-pagination')
+const PAGINATION_OFFSET = 7
 
 const createPosts = (createPage, createRedirect, edges) => {
   edges.forEach(({ node }, i) => {
-    const prev = i === 0 ? null : edges[i - 1].node;
-    const next = i === edges.length - 1 ? null : edges[i + 1].node;
-    const pagePath = node.fields.slug;
+    const prev = i === 0 ? null : edges[i - 1].node
+    const next = i === edges.length - 1 ? null : edges[i + 1].node
+    const pagePath = node.fields.slug
 
     if (node.fields.redirects) {
       node.fields.redirects.forEach(fromPath => {
@@ -17,8 +17,8 @@ const createPosts = (createPage, createRedirect, edges) => {
           toPath: pagePath,
           redirectInBrowser: true,
           isPermanent: true,
-        });
-      });
+        })
+      })
     }
 
     createPage({
@@ -29,9 +29,9 @@ const createPosts = (createPage, createRedirect, edges) => {
         prev,
         next,
       },
-    });
-  });
-};
+    })
+  })
+}
 
 exports.createPages = ({ actions, graphql }) =>
   graphql(`
@@ -64,50 +64,41 @@ exports.createPages = ({ actions, graphql }) =>
     }
   `).then(({ data, errors }) => {
     if (errors) {
-      return Promise.reject(errors);
+      return Promise.reject(errors)
     }
 
-    const { edges } = data.allMdx;
-    const { createRedirect, createPage } = actions;
-    createPosts(createPage, createRedirect, edges);
+    const { edges } = data.allMdx
+    const { createRedirect, createPage } = actions
+    createPosts(createPage, createRedirect, edges)
     createPaginatedPages(actions.createPage, edges, '/blog', {
       categories: [],
-    });
-  });
+    })
+  })
 
 exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     resolve: {
       modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-      alias: {
-        $components: path.resolve(__dirname, 'src/components'),
-      },
     },
-  });
-};
+  })
+}
 
-const createPaginatedPages = (
-  createPage,
-  edges,
-  pathPrefix,
-  context,
-) => {
+const createPaginatedPages = (createPage, edges, pathPrefix, context) => {
   const pages = edges.reduce((acc, value, index) => {
-    const pageIndex = Math.floor(index / PAGINATION_OFFSET);
+    const pageIndex = Math.floor(index / PAGINATION_OFFSET)
 
     if (!acc[pageIndex]) {
-      acc[pageIndex] = [];
+      acc[pageIndex] = []
     }
 
-    acc[pageIndex].push(value.node.id);
+    acc[pageIndex].push(value.node.id)
 
-    return acc;
-  }, []);
+    return acc
+  }, [])
 
   pages.forEach((page, index) => {
-    const previousPagePath = `${pathPrefix}/${index + 1}`;
-    const nextPagePath =
-      index === 1 ? pathPrefix : `${pathPrefix}/${index - 1}`;
+    const previousPagePath = `${pathPrefix}/${index + 1}`
+    const nextPagePath = index === 1 ? pathPrefix : `${pathPrefix}/${index - 1}`
 
     createPage({
       path: index > 0 ? `${pathPrefix}/${index}` : `${pathPrefix}`,
@@ -123,87 +114,82 @@ const createPaginatedPages = (
         },
         ...context,
       },
-    });
-  });
-};
+    })
+  })
+}
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions;
+  const { createNodeField } = actions
 
   if (node.internal.type === `Mdx`) {
-    const parent = getNode(node.parent);
-    const titleSlugged = _.join(
-      _.drop(parent.name.split('-'), 3),
-      '-',
-    );
+    const parent = getNode(node.parent)
+    const titleSlugged = _.join(_.drop(parent.name.split('-'), 3), '-')
 
     const slug =
       parent.sourceInstanceName === 'legacy'
         ? `blog/${node.frontmatter.date
             .split('T')[0]
             .replace(/-/g, '/')}/${titleSlugged}`
-        : node.frontmatter.slug || titleSlugged;
+        : node.frontmatter.slug || titleSlugged
 
     createNodeField({
       name: 'id',
       node,
       value: node.id,
-    });
+    })
 
     createNodeField({
       name: 'published',
       node,
       value: node.frontmatter.published,
-    });
+    })
 
     createNodeField({
       name: 'title',
       node,
       value: node.frontmatter.title,
-    });
+    })
 
     createNodeField({
       name: 'description',
       node,
       value: node.frontmatter.description,
-    });
+    })
 
     createNodeField({
       name: 'slug',
       node,
       value: slug,
-    });
+    })
 
     createNodeField({
       name: 'date',
       node,
-      value: node.frontmatter.date
-        ? node.frontmatter.date.split(' ')[0]
-        : '',
-    });
+      value: node.frontmatter.date ? node.frontmatter.date.split(' ')[0] : '',
+    })
 
     createNodeField({
       name: 'banner',
       node,
       banner: node.frontmatter.banner,
-    });
+    })
 
     createNodeField({
       name: 'categories',
       node,
       value: node.frontmatter.categories || [],
-    });
+    })
 
     createNodeField({
       name: 'keywords',
       node,
       value: node.frontmatter.keywords || [],
-    });
+    })
 
     createNodeField({
       name: 'redirects',
       node,
       value: node.frontmatter.redirects,
-    });
+    })
   }
-};
+}
