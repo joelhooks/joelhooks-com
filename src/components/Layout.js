2c106dc8ef8079e6e3f4be8b58d01168cb2e34da
Joel Hooks <joelhooks@gmail.com>
2019-01-26 15:39:51 -0800

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -20,11 +20,6 @@ export const globalStyles = css`
     background: ${theme.colors.primary_light};
   }
   ${bpMaxSM} {
-    p,
-    em,
-    strong {
-      font-size: 90%;
-    }
     h1 {
       font-size: 30px;
     }
