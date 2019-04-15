4139c395ffab2397879167ac46084f5a1cca8d87
evgeniy.nagalskiy <evgeniy.nagalskiy@gmail.com>
2019-04-15 22:12:15 +0300

    fix

--- src/components/ThemeToggler.js
+++ src/components/ThemeToggler.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 const ThemeToggler = ({ toggleTheme, themeName }) => (
   <button
+    style={{ width: '100px' }}
     onClick={() => toggleTheme(themeName === 'dark' ? 'default' : 'dark')}
   >
     {themeName === 'dark' ? 'default' : 'dark'}
