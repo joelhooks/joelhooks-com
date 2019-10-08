e42a0b76661eae56821a7697656fed7e7aa0d900
Vojta Holik <vojta@egghead.io>
2019-10-08 22:40:46 +0200

    Add candy (#15)
    * tweak share widget
    
    * bump article h1 font size
    
    * add a bunch of fun vector illustrations

--- src/components/Share.js
+++ src/components/Share.js
@@ -1,63 +1,65 @@
 import React from 'react'
 import { css } from '@emotion/core'
-
-import colors from '../lib/colors'
 import { useTheme } from './Theming'
+import fonts from '../lib/typography'
 
 import { TwitterIcon, TwitterShareButton } from 'react-share'
+import SparklingCandy from './SparklingCandy'
 
 const Share = ({ url, title, twitterHandle }) => {
   const theme = useTheme()
   return (
     <div>
       <div
-        css={css`
-          display: flex;
-          align-items: center;
-          flex-direction: column;
-          justify-content: center;
-        `}
+        css={{
+          margin: '3rem auto',
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          textAlign: 'center',
+          maxWidth: '25rem',
+        }}
       >
-        <div
+        <SparklingCandy />
+
+        <TwitterShareButton
+          url={url}
+          title={title}
+          via={twitterHandle.split('@').join('')}
           css={css`
-            padding-bottom: 25px;
+            cursor: pointer;
+            :hover {
+              color: ${theme.colors.primary};
+            }
           `}
         >
-          <TwitterShareButton
-            url={url}
-            title={title}
-            via={twitterHandle.split('@').join('')}
-            css={css`
-              cursor: pointer;
-              :hover {
-                color: ${theme.colors.primary};
-              }
-            `}
+          <h2
+            css={{
+              margin: '1rem 0',
+              fontFamily: fonts.semibold,
+              span: {
+                color: theme.colors.primary,
+              },
+            }}
           >
-            <div
-              css={css`
-                padding-left: 10px;
-              `}
-            >
-              <strong>
-                ✨✨ click here to share this article with your friends on
-                Twitter ✨✨
-              </strong>
-            </div>
-          </TwitterShareButton>
-        </div>
-        <div
-          css={css`
-            padding-top: 10px;
-            opacity: 0.7;
-            max-width: 75%;
-            font-size: 12px;
-          `}
+            <span>Click here</span> to share this article with your friends on
+            Twitter.
+          </h2>
+        </TwitterShareButton>
+
+        <small
+          css={{
+            opacity: 0.7,
+          }}
         >
           <strong>This site isn't collecting analytics</strong>. 0. It's a huge
           help to me if you share it on Twitter if you enjoy it. That way other
-          people can also enjoy it and I notice that it was enjoyed. 🙏
-        </div>
+          people can also enjoy it and I notice that it was enjoyed.{' '}
+          <span role="img" aria-label="praying hands">
+            🙏
+          </span>
+        </small>
       </div>
     </div>
   )
