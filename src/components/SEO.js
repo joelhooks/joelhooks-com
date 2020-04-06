4d76007207017eeddc8729aaf27db580f6c3f0bb
Joel Hooks <joelhooks@gmail.com>
2020-04-05 18:05:19 -0700

    bump the version

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -45,7 +45,7 @@ const SEO = ({postData, excerpt, frontmatter = {}, postImage, isBlogPost}) => (
       const {fbAppID, twitter} = seo.social
       const ogImage = `https://competent-goodall-d71d0d.netlify.app/opengraph?title=${encodeURIComponent(
         title,
-      )}&author=${encodeURIComponent(twitter)}&v=0.0.7`
+      )}&author=${encodeURIComponent(twitter)}&v=0.0.8`
 
       return (
         <React.Fragment>
